05f588d7e66eddc6f31b872dea220dc0f0373234
mariojacob <mailto.mario@gmail.com>
2020-11-02 12:25:39 +0100

    Bot user agents Liste aktualisiert

--- lib/bot_user_agents.php
+++ lib/bot_user_agents.php
@@ -15,6 +15,7 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/5.0 (compatible; AhrefsBot/5.1; +http://ahrefs.com/robot/)', // Ahrefs Backlink Research Bot 5.1
     'Mozilla/5.0 (compatible; AhrefsBot/5.2; +http://ahrefs.com/robot/)', // Ahrefs Backlink Research Bot 5.2
     'Mozilla/5.0 (compatible; AhrefsBot/6.1; +http://ahrefs.com/robot/)', // Ahrefs Backlink Research Bot 6.1
+    'Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)', // Ahrefs Backlink Research Bot 7.0
     // Alexa
     'ia_archiver', // Alexa Bot
     'ia_archiver-web.archive.org', // Alexa Bot
@@ -52,6 +53,9 @@ define('ZDM__BOT_USER_AGENTS', [
     'msnbot/2.0b (+http://search.msn.com/msnbot.htm)', // MSN Bot 2.0b
     'msnbot-media/1.0 (+http://search.msn.com/msnbot.htm)', // MSN Media Bot 1.0
     'msnbot-media/1.1 (+http://search.msn.com/msnbot.htm)', // MSN Media Bot 1.1
+    // Daum Bot
+    'Mozilla/5.0 (compatible; Daum/4.1; +http://cs.daum.net/faq/15/4118.html?faqId=28966)', // Daum Bot
+    'Mozilla/5.0 (Unknown; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) Safari/538.1 Daum/4.1', , // Daum Bot
     // DuckDuckGo
     'DuckDuckBot/1.0; (+http://duckduckgo.com/duckduckbot.html)', // DuckDuckGo
     'Mozilla/5.0 (compatible; DuckDuckGo-Favicons-Bot/1.0; +http://duckduckgo.com)', // DuckDuckGo Favicons Bot 1.0
@@ -121,6 +125,12 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/5.0 (compatible; MJ12bot/v1.4.5; http://www.majestic12.co.uk/bot.php?+)', // Majestic-12 Distributed Search Bot 1.4
     'Mozilla/5.0 (compatible; MJ12bot/v1.4.7; http://mj12bot.com/)', // Majestic-12 Distributed Search Bot 1.4
     'Mozilla/5.0 (compatible; MJ12bot/v1.4.8; http://mj12bot.com/)', // Majestic-12 Distributed Search Bot 1.4
+    // MozacFetch
+    'MozacFetch/57.0.7', // MozacFetch
+    'MozacFetch/57.0.8', // MozacFetch
+    // PetalBot
+    'Mozilla/5.0 (compatible;PetalBot;+https://aspiegel.com/petalbot)',
+    'Mozilla/5.0 (Linux; Android 7.0;) AppleWebKit/537.36 (KHTML, like Gecko) Mobile Safari/537.36 (compatible; PetalBot;+https://aspiegel.com/petalbot)',
     // Pinterest
     'Pinterest/0.2 (+http://www.pinterest.com/)', // Pinterest Bot 0.2
     'Mozilla/5.0 (compatible; Pinterestbot/1.0; +http://www.pinterest.com/bot.html)', // Pinterest Bot
@@ -143,6 +153,9 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/2.0 (compatible; Ask Jeeves/Teoma; +http://about.ask.com/en/docs/about/webmasters.shtml)', // Ask Jeeves Crawler
     'Mozilla/2.0 (compatible; Ask Jeeves/Teoma)', // Ask Jeeves Crawler
     'Mozilla/5.0 (compatible; Ask Jeeves/Teoma; +http://about.ask.com/en/docs/about/webmasters.shtml)', // Ask Jeeves Crawler
+    // Turnitin
+    'Turnitin (https://bit.ly/2UvnfoQ)', // Turnitin
+    'TurnitinBot (https://turnitin.com/robot/crawlerinfo.html)', // TurnitinBot
     // Twitter
     'Twitterbot/1.0', // TwitterBot 1.0'
     // Voila
@@ -176,7 +189,9 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/5.0 (compatible; YandexVertis/3.0; +http://yandex.com/bots)', // Yandex
     'Mozilla/5.0 (compatible; YandexBot/3.0; MirrorDetector; +http://yandex.com/bots)', // Yandex
     // other
+    'Mozilla/5.0 (compatible; Adsbot/3.1)', // Adsbot
     'Mozilla/5.0 (compatible; adscanner/)', // AdScanner Crawler
+    'Mozilla/5.0 (compatible; Barkrowler/0.9; +https://babbar.tech/crawler)', // Barkrowler 0.9
     'Mozilla/5.0 (compatible; BecomeBot/3.0; +http://www.become.com/site_owners.html)', // Become.com Crawler 3
     'Mozilla/5.0 (compatible; ScoutJet; +http://www.scoutjet.com/)', // Blekko Scoutjet Crawler
     'Mozilla/5.0 (compatible; BLEXBot/1.0; +http://webmeup-crawler.com/)', // BLEXBot Crawler 1.0
@@ -190,16 +205,20 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Exabot-Thumbnails)', // Exabot
     'Mozilla/5.0 (compatible; Exabot/3.0; +http://www.exabot.com/go/robot)', // ExaLead Crawler 3
     'Mozilla/5.0 (compatible; ExaleadCloudView/5;)', // Exalead CloudView Crawler 5
+    'FDM 3.x', // FDM
     'Mozilla/5.0 (compatible; special_archiver/3.1.1 +http://www.archive.org/details/archive.org_bot)', // Internet Archiver Bot
     'Mozilla/5.0 (compatible; archive.org_bot +http://www.archive.org/details/archive.org_bot)', // Internet Archiver Bot
     'Mozilla/5.0 (compatible; memoryBot/1.21.24 +http://internetmemory.org/en/)', // Internet Memory Crawler 1.21
     'Jyxobot/1', // Jyxo.cz Crawler 1.0
+    'Linguee Bot (http://www.linguee.com/bot; bot@linguee.com)', // Linguee Bot
     'Mozilla/5.0 (compatible; Linux x86_64; Mail.RU_Bot/Fast/2.0; +http://go.mail.ru/help/robots)', // Mail.ru Crawler
     'Mozilla/5.0 (compatible; MegaIndex.ru/2.0; +http://megaindex.com/crawler)', // MegaIndex Crawler 2.0
+    'netEstate NE Crawler (+http://www.website-datenbank.de/)', // netEstate NE Crawler
     'Mozilla/5.0 (compatible; DotBot/1.1; http://www.opensiteexplorer.org/dotbot, help@moz.com)', // OpenSiteExplorer Crawler
     'Mozilla/5.0 (compatible; openstat.ru/Bot)', // Openstat.com Crawler
     'phpcrawl', // PHPCrawl
     'psbot/0.1 (+http://www.picsearch.com/bot.html)', // Picsearch Crawler 0.1
+    'pimeyes.com crawler', // PimEyes
     'Mozilla/5.0 (compatible; proximic; +https://www.comscore.com/Web-Crawler)', // Proximic | Search Engine | Desktop
     'Mozilla/5.0 (compatible; Qwantify/2.4w; +https://www.qwant.com/)/2.4w', // Qwantify Search Crawler 2.4w
     'Screaming Frog SEO Spider/6.2', // Screaming Frog Crawler 6.2
@@ -217,4 +236,7 @@ define('ZDM__BOT_USER_AGENTS', [
     'Mozilla/5.0 (compatible; TTD-Content; +https://www.thetradedesk.com/general/ttd-content)', // The Trade Desk Content Scraper
     'Mozilla/5.0 (compatible; vebidoobot/1.0; +https://blog.vebidoo.de/vebidoobot/)', // vebidoobot
     'ZoominfoBot (zoominfobot at zoominfo dot com)', // Zoom Info Bot
+    // temp
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15',
 ]);
