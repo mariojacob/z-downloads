29b5739110434fc494b9c9da8f157977e59476f4
mariojacob <mailto.mario@gmail.com>
2021-12-03 12:54:03 +0100

    Update Funktion aktualisiert.

--- lib/upgrade.php
+++ lib/upgrade.php
@@ -8,16 +8,14 @@ $zdm_options = get_option('zdm_options');
 if ($zdm_options['version'] < ZDM__VERSION) {
 
     // NOTE: since v1.9.0
-    if (ZDM__VERSION >= '1.9.0') {
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'zdm_log';
-        $drop_ddl = "ALTER TABLE " . $table_name . " DROP `user_id`";
-        require_once(ABSPATH . 'wp-admin/install-helper.php');
-        maybe_drop_column($table_name, "user_id", $drop_ddl);
-
-        flush_rewrite_rules();
-        wp_cache_flush();
-    }
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'zdm_log';
+    $drop_ddl = "ALTER TABLE " . $table_name . " DROP `user_id`";
+    require_once(ABSPATH . 'wp-admin/install-helper.php');
+    maybe_drop_column($table_name, "user_id", $drop_ddl);
+    flush_rewrite_rules();
+    wp_cache_flush();
+    // since v1.9.0 end
 
     ZDMCore::log('plugin upgrade', $zdm_options['version'] . ' to ' . ZDM__VERSION);
 
