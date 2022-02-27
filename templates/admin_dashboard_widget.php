9f8b220bae7a5ad3e4dbe3f6b8634c8c631dca98
mariojacob <mailto.mario@gmail.com>
2022-02-27 09:45:23 +0100

    Doppelte Vorzeichen im Dashboard Widget behoben.

--- templates/admin_dashboard_widget.php
+++ templates/admin_dashboard_widget.php
@@ -4,7 +4,6 @@
 $zdm_statistics_last_2_days = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400 * 2));
 $zdm_statistics_last_1_day = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400));
 $zdm_statistics_last_1_day_before = $zdm_statistics_last_2_days - $zdm_statistics_last_1_day;
-$zdm_statistics_last_1_day_trend_sign = '';
 if ($zdm_statistics_last_1_day_before == 0) {
     $zdm_statistics_last_1_day_trend = '--';
     $zdm_statistics_last_1_day_trend_class = 'zdm-color-grey11';
@@ -18,18 +17,20 @@ if ($zdm_statistics_last_1_day_before == 0) {
     $zdm_statistics_last_1_day_trend = sprintf($zdm_statistics_last_1_day_trend);
     if ($zdm_statistics_last_1_day_trend > 0) {
         $zdm_statistics_last_1_day_trend_class = 'zdm-color-green';
-        $zdm_statistics_last_1_day_trend_sign = '+';
+        $zdm_statistics_last_1_day_trend = '+' . $zdm_statistics_last_1_day_trend;
     } else {
         $zdm_statistics_last_1_day_trend_class = 'zdm-color-red';
-        $zdm_statistics_last_1_day_trend_sign = '-';
+        $zdm_statistics_last_1_day_trend = '-' . $zdm_statistics_last_1_day_trend;
     }
+
+    $zdm_statistics_last_1_day_trend = str_replace('--', '-', $zdm_statistics_last_1_day_trend);
+    $zdm_statistics_last_1_day_trend = str_replace('++', '+', $zdm_statistics_last_1_day_trend);
 }
 
 // Last 7 days
 $zdm_statistics_last_14_days = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400 * 14));
 $zdm_statistics_last_7_days = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400 * 7));
 $zdm_statistics_last_7_days_before = $zdm_statistics_last_14_days - $zdm_statistics_last_7_days;
-$zdm_statistics_last_7_days_trend_sign = '';
 if ($zdm_statistics_last_7_days_before == 0) {
     $zdm_statistics_last_7_days_trend = '--';
     $zdm_statistics_last_7_days_trend_class = 'zdm-color-grey11';
@@ -43,18 +44,20 @@ if ($zdm_statistics_last_7_days_before == 0) {
     $zdm_statistics_last_7_days_trend = sprintf($zdm_statistics_last_7_days_trend);
     if ($zdm_statistics_last_7_days_trend > 0) {
         $zdm_statistics_last_7_days_trend_class = 'zdm-color-green';
-        $zdm_statistics_last_7_days_trend_sign = '+';
+        $zdm_statistics_last_7_days_trend = '+' . $zdm_statistics_last_7_days_trend;
     } else {
         $zdm_statistics_last_7_days_trend_class = 'zdm-color-red';
-        $zdm_statistics_last_7_days_trend_sign = '-';
+        $$zdm_statistics_last_7_days_trend = '-' . $zdm_statistics_last_7_days_trend;
     }
+
+    $zdm_statistics_last_7_days_trend = str_replace('--', '-', $zdm_statistics_last_7_days_trend);
+    $zdm_statistics_last_7_days_trend = str_replace('++', '+', $zdm_statistics_last_7_days_trend);
 }
 
 // Last 30 days
 $zdm_statistics_last_60_days = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400 * 60));
 $zdm_statistics_last_30_days = ZDMCore::number_format(ZDMStat::get_downloads_count_time('all', 86400 * 30));
 $zdm_statistics_last_30_days_before = $zdm_statistics_last_60_days - $zdm_statistics_last_30_days;
-$zdm_statistics_last_30_days_trend_sign = '';
 if ($zdm_statistics_last_30_days_before == 0) {
     $zdm_statistics_last_30_days_trend = '--';
     $zdm_statistics_last_30_days_trend_class = 'zdm-color-grey11';
@@ -68,11 +71,14 @@ if ($zdm_statistics_last_30_days_before == 0) {
     $zdm_statistics_last_30_days_trend = sprintf($zdm_statistics_last_30_days_trend);
     if ($zdm_statistics_last_30_days_trend > 0) {
         $zdm_statistics_last_30_days_trend_class = 'zdm-color-green';
-        $zdm_statistics_last_30_days_trend_sign = '+';
+        $zdm_statistics_last_30_days_trend = '+' . $zdm_statistics_last_30_days_trend;
     } else {
         $zdm_statistics_last_30_days_trend_class = 'zdm-color-red';
-        $zdm_statistics_last_30_days_trend_sign = '-';
+        $zdm_statistics_last_30_days_trend = '-' . $zdm_statistics_last_30_days_trend;
     }
+
+    $zdm_statistics_last_30_days_trend = str_replace('--', '-', $zdm_statistics_last_30_days_trend);
+    $zdm_statistics_last_30_days_trend = str_replace('++', '+', $zdm_statistics_last_30_days_trend);
 }
 
 ?>
@@ -80,11 +86,11 @@ if ($zdm_statistics_last_30_days_before == 0) {
 <div class="zdm_dashboard_widget">
     <ul>
         <li><span class="zdm_dashboard_widget_leading"><?= esc_html__('Last 24 hours', 'zdm') ?>:</span></li>
-        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_1_day ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_1_day_trend_class ?>">(<?= $zdm_statistics_last_1_day_trend_sign ?><?= $zdm_statistics_last_1_day_trend ?>%)</span></li>
+        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_1_day ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_1_day_trend_class ?>">(<?= $zdm_statistics_last_1_day_trend ?>%)</span></li>
         <li><span class="zdm_dashboard_widget_leading"><?= esc_html__('Last 7 days', 'zdm') ?>:</span></li>
-        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_7_days ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_7_days_trend_class ?>">(<?= $zdm_statistics_last_7_days_trend_sign ?><?= $zdm_statistics_last_7_days_trend ?>%)</span></li>
+        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_7_days ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_7_days_trend_class ?>">(<?= $zdm_statistics_last_7_days_trend ?>%)</span></li>
         <li><span class="zdm_dashboard_widget_leading"><?= esc_html__('Last 30 days', 'zdm') ?>:</span></li>
-        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_30_days ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_30_days_trend_class ?>">(<?= $zdm_statistics_last_30_days_trend_sign ?><?= $zdm_statistics_last_30_days_trend ?>%)</span></li>
+        <li><span class="zdm_dashboard_widget_number zdm-color-primary"><?= $zdm_statistics_last_30_days ?></span>&nbsp;&nbsp;<span class="zdm_dashboard_widget_number zdm_dashboard_widget_number_trend <?= $zdm_statistics_last_30_days_trend_class ?>">(<?= $zdm_statistics_last_30_days_trend ?>%)</span></li>
         <li class="zdm_dashboard_widget_mb"><span class="zdm_dashboard_widget_leading"><?= esc_html__('Total', 'zdm') ?>:</span></li>
         <li class="zdm_dashboard_widget_mb"><span class="zdm_dashboard_widget_number zdm-color-primary"><?= ZDMCore::number_format(ZDMStat::get_downloads_count('all')) ?></span></li>
     </ul>
